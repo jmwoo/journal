ef604e9854037fb51de3dc6d551208e54b297472
Jimmy Woolley <jamesmwoolley@gmail.com>
2021-10-18 13:44:29 -0700

    Modernize (#2)
    * changing to separate files with classes
    
    * working ok
    
    * working
    
    * change readme
    
    * bold

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { getJournal } from './journal'
+import { PrintDirection } from './types'
+import yargs from 'yargs'
+import readline from 'readline'
+
+async function main() {
+	const args = await yargs(process.argv.slice(2)).options({
+		journal: {type: 'string', default: 'main', alias: 'j'},
+		write: {type: 'boolean', default: false, alias: 'w'},
+		print: {type: 'boolean', default: false, alias: 'p'},
+		search: {type: 'string', alias: 's' },
+		first: {type: 'number', alias: 'f'},
+		last: {type: 'number', alias: 'l'}
+	}).parse()
+
+	const journal = await getJournal(args.journal)
+
+	if (args.print) {
+		journal.print({
+			printDirection: args.first ? PrintDirection.Front : PrintDirection.Back,
+			amount: (args.first || args.last || Number.MAX_SAFE_INTEGER)
+		})
+	}
+	else if (args.search && args.search.trim() != '') {
+		journal.search(args.search)
+	}
+	else if (args.write) {
+		const rl = readline.createInterface(process.stdin, process.stdout)
+		const setPrompt = () => {
+			rl.setPrompt(`'${journal.getName()}' (${journal.getNextId()}) >>> `)
+			rl.prompt()
+		}
+		rl.on('line', async (text) => {
+			text = text.trim()
+			if (text) {
+				await journal.addEntry(text)
+			}
+			setPrompt()
+		})
+		setPrompt()
+	}
+}
+
+main()
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface Entry {
+	timestamp: string,
+	id: number,
+	text: string
+}
+
+export interface JouralArguments {
+	journalName: string,
+	directoryName: string,
+	pathName: string,
+	entries: Entry[]
+}
+
+export interface PrintOptions {
+	printDirection: PrintDirection
+	amount: number,
+}
+
+export enum PrintDirection {
+	Front,
+	Back
+}
