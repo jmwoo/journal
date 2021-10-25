1572b048761834e8fabed3d765aed4b14c2351b7
Jimmy <jamesmwoolley@gmail.com>
2021-10-25 15:09:14 -0600

    add word frequency metric

---
+++ src/metrics.ts
@@ -0,0 +1,26 @@
+import { Entry, IOutput } from "./types"
+
+export function getWordFrequency(entries: Entry[], output: IOutput): void {
+    const wordCount = new Map<string, number>()
+    const punctuation = ['.', ',', '!', '"', ';', ':', "'", '(', ')']
+    for (const entry of entries) {
+        entry.text.split(' ').map(word => {
+            word = word.toLowerCase()
+            for (const p of punctuation) {
+                word = word.replace(p, '')
+            }
+            const count = wordCount.get(word) ?? 0;
+            wordCount.set(word, count + 1)
+        })
+    }
+    const sortedWordCount = new Map([...wordCount.entries()].sort((a, b) => b[1] - a[1]))
+
+    let index = 0
+    sortedWordCount.forEach((value, key) => {
+        output.out(`${++index}) ${key}: ${value}`)
+    })
+}
+
+export function getTimeOfDayFrequency(entries: Entry[], output: IOutput): void {
+    throw new Error('not implemented')
+}
